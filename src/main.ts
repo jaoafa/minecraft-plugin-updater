5938d6741e1a928220f6735c130287857e17f131
Tomachi [ICHIGO] <tomachi@tomacheese.com>
2022-08-30 14:15:32 +0900

    feat: バージョン変更一覧表を出すためのdiff

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import {PluginYml} from "./lib/PluginYml"
+import Plugin from '@/lib/Plugin'
+import updatePlugin from '@/lib/UpdatePlugin'
 import fs from 'fs'
-import Plugin from "@/lib/Plugin";
-import * as readline from "readline";
-import updatePlugin from "@/lib/UpdatePlugin";
+import * as readline from 'readline'
+import { PluginYml } from './lib/PluginYml'
 
 function getOneLineInput(question: string): Promise<string> {
   return new Promise<string>((resolve) => {
@@ -14,11 +14,11 @@ function getOneLineInput(question: string): Promise<string> {
       resolve(answer)
       readlineInterface.close()
     })
-  });
+  })
 }
 
-async function main() {
-  const serverVersion = await getOneLineInput("server version: ")
+async function mainUpdating() {
+  const serverVersion = await getOneLineInput('server version: ')
 
   const dir = process.env.OLD_PLUGIN_DIR || 'plugins-old/'
   const newDir = process.env.NEW_PLUGIN_DIR || 'plugins/'
@@ -27,35 +27,88 @@ async function main() {
   }
   fs.mkdirSync(newDir)
   const pluginsFiles = fs.readdirSync(dir)
-  console.log("Plugin files:", pluginsFiles.length)
+  console.log('Plugin files:', pluginsFiles.length)
   for (const pluginFile of pluginsFiles) {
     const path = `${dir}${pluginFile}`
     const pluginYml = new PluginYml(path)
     const plugin = Plugin(pluginYml.main)
     if (!plugin) {
       console.warn(`Plugin ${pluginYml.name} is not found.`)
-      console.log("Copy to new directory")
+      console.log('Copy to new directory')
       fs.copyFileSync(path, `${newDir}${pluginFile}`)
       continue
     }
 
-    console.log("Name:", pluginYml.name)
-    console.log("Now version:", pluginYml.version)
+    console.log('Name:', pluginYml.name)
+    console.log('Now version:', pluginYml.version)
 
     const result = await plugin.package.parse()
-    console.log("Latest version:", result.latest.version)
-    console.log("")
-    const answer = await getOneLineInput("Do you want to update? (y/n): ")
-    if (answer === "y") {
+    console.log('Latest version:', result.latest.version)
+    console.log('')
+    const answer = await getOneLineInput('Do you want to update? (y/n): ')
+    if (answer === 'y') {
       await updatePlugin(plugin, result, serverVersion)
-    }else{
-      console.log("Copy to new directory")
+    } else {
+      console.log('Copy to new directory')
       fs.copyFileSync(path, `${newDir}${pluginFile}`)
     }
-    console.log("----------------------------------------------------")
+    console.log('----------------------------------------------------')
   }
 }
 
-(async () => {
-  await main()
+async function mainDiff() {
+  const dir = process.env.OLD_PLUGIN_DIR || 'plugins-old/'
+  const newDir = process.env.NEW_PLUGIN_DIR || 'plugins/'
+  if (!fs.existsSync(dir)) {
+    console.error('Old directory is not found')
+    return
+  }
+  if (!fs.existsSync(newDir)) {
+    console.error('New directory not found')
+    return
+  }
+  const pluginsFiles = fs.readdirSync(dir).map((p) => {
+    return new PluginYml(`${dir}${p}`)
+  })
+  const newPluginsFiles = fs.readdirSync(newDir).map((p) => {
+    return new PluginYml(`${newDir}${p}`)
+  })
+  console.log(
+    'Plugin files:',
+    pluginsFiles.length,
+    '->',
+    newPluginsFiles.length
+  )
+
+  const diff = pluginsFiles.filter((p) => {
+    return newPluginsFiles.find((np) => np.main === p.main)
+  })
+
+  console.log('Diff:', diff.length)
+  console.log('----------------------------------------------------')
+
+  for (const plugin of diff) {
+    const newPlugin = newPluginsFiles.find(
+      (np) => np.main === plugin.main
+    ) as PluginYml
+    if (plugin.version === newPlugin.version) {
+      continue
+    }
+    console.log(`${plugin.name}: ${plugin.version} -> ${newPlugin.version}`)
+  }
+  console.log('----------------------------------------------------')
+}
+
+;(async () => {
+  const target = await getOneLineInput('Select target(update or diff): ')
+  switch (target) {
+    case 'update':
+      await mainUpdating()
+      break
+    case 'diff':
+      await mainDiff()
+      break
+    default:
+      console.error('Invalid target')
+  }
 })()
